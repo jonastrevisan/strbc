1d6d9f5726d673a16508d5c55e77939cad9974d6
Jonas <Jonas>
2017-11-13 21:48:39 -0200

    criado develop

--- inicio2.php
+++ inicio2.php
@@ -29,7 +29,7 @@ include("php/verifica.php");
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })(); 
-
+ 
 </script>
 </head>
 
